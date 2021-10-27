210e89cc1909585434b59e10c21ee601b8106e00
zakz7788 <fino6462@gmail.com>
2021-10-27 08:12:23 +0700

    ngetes doang bang

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY Zakz Ganz
+
+*さ BANK*
+1. BRI 
+	ㅁ -
+		• -
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0857 3174 9158
+		• MU***KY
+2. OVO
+	ㅁ 0857 3174 9158
+		• MU***KY
+3. DANA
+	ㅁ 0857 3174 9158
+		• MU***KY
+
+*さ PULSA*
+1. INDOSAT
+	ㅁ 0857 3174 9158
+2. THREE
+	ㅁ 0895 1447 7867
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
